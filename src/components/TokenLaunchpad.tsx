893c217eb136536a9a749937bd8c2a3c15825c1d
shivdotsol <defi.shiv@gmail.com>
2025-09-11 14:00:51 +0530

    v2 - can create tokens with metadata

--- src/components/TokenLaunchpad.tsx
+++ src/components/TokenLaunchpad.tsx
@@ -1,14 +1,26 @@
 import {
+  createAssociatedTokenAccountInstruction,
+  createInitializeMetadataPointerInstruction,
   createInitializeMint2Instruction,
+  createInitializeMintInstruction,
+  createMintToInstruction,
+  ExtensionType,
+  getAssociatedTokenAddressSync,
   getMinimumBalanceForRentExemptMint,
+  getMintLen,
+  LENGTH_SIZE,
   MINT_SIZE,
+  TOKEN_2022_PROGRAM_ID,
   TOKEN_PROGRAM_ID,
+  TYPE_SIZE,
 } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { Upload } from "lucide-react";
-import { useState, type InputEvent } from "react";
+import { useState } from "react";
 import axios from "axios";
+import { uploadMetadataToIPFS, type MetadataObject } from "../libs/utils";
+import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 
 function TokenLaunchpad() {
   const { connection } = useConnection();
@@ -16,8 +28,10 @@ function TokenLaunchpad() {
   const [tokenName, setTokenName] = useState("");
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [tokenImageFile, setTokenImageFile] = useState<File | null>();
+  const [tokenDescription, setTokenDescription] = useState("");
   const [tokenSupply, setTokenSupply] = useState<number>();
   const [tokenDecimals, setTokenDecimals] = useState<number>();
+  const [status, setStatus] = useState("");
 
   const handleImageFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -28,32 +42,124 @@ function TokenLaunchpad() {
   const handleCreateToken = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!wallet) throw new Error("wallet not found");
+    if (!wallet || !wallet.publicKey) {
+      setStatus("wallet not connected");
+      throw new Error("wallet not found");
+    }
+    if (!tokenImageFile) {
+      setStatus("Token image not found");
+      throw new Error("no image found");
+    }
 
-    const lamports = await getMinimumBalanceForRentExemptMint(connection);
-    const keyPair = Keypair.generate();
-    const transaction = new Transaction();
-    transaction.add(
+    const tokenMetadata: MetadataObject = {
+      name: tokenName,
+      symbol: tokenSymbol,
+      image: "", // this will be set inside the uploadMetadataToIPFS() function
+      description: tokenDescription,
+    };
+
+    const uploadRes = await uploadMetadataToIPFS({
+      publicKey: wallet.publicKey!.toString(),
+      metadata: tokenMetadata,
+      imageFile: tokenImageFile,
+    });
+    if (!uploadRes.cid) {
+      setStatus(uploadRes.message);
+      return;
+    }
+    const metadataUri = `https://ipfs.io/ipfs/${uploadRes.cid}`;
+
+    //////////////////////////////////////////////////////////////////
+
+    const mintKeypair = Keypair.generate();
+    const metadata = {
+      mint: mintKeypair.publicKey,
+      name: tokenName,
+      symbol: tokenSymbol,
+      uri: metadataUri,
+      additionalMetadata: [],
+    };
+    const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+    const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      mintLen + metadataLen
+    );
+
+    const transaction = new Transaction().add(
       SystemProgram.createAccount({
-        fromPubkey: wallet.publicKey!,
-        newAccountPubkey: keyPair.publicKey,
-        space: MINT_SIZE,
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mintKeypair.publicKey,
+        space: mintLen,
         lamports,
-        programId: TOKEN_PROGRAM_ID,
+        programId: TOKEN_2022_PROGRAM_ID,
       }),
-      createInitializeMint2Instruction(
-        keyPair.publicKey,
-        tokenDecimals!,
-        wallet.publicKey!,
+      createInitializeMetadataPointerInstruction(
+        mintKeypair.publicKey,
         wallet.publicKey,
-        TOKEN_PROGRAM_ID
+        wallet.publicKey,
+        TOKEN_2022_PROGRAM_ID
+      ),
+      createInitializeMintInstruction(
+        mintKeypair.publicKey,
+        tokenDecimals!,
+        wallet.publicKey,
+        null,
+        TOKEN_2022_PROGRAM_ID
+      ),
+      createInitializeInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        mint: mintKeypair.publicKey,
+        metadata: mintKeypair.publicKey,
+        name: tokenName,
+        symbol: tokenSymbol,
+        uri: metadataUri,
+        mintAuthority: wallet.publicKey,
+        updateAuthority: wallet.publicKey,
+      })
+    );
+
+    transaction.feePayer = wallet.publicKey;
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+    transaction.partialSign(mintKeypair);
+    await wallet.sendTransaction(transaction, connection);
+
+    /////////////////////////////////////////////////////////////
+
+    // minting to user's ATA
+
+    const associatedTokenAddress = getAssociatedTokenAddressSync(
+      mintKeypair.publicKey,
+      wallet.publicKey,
+      false,
+      TOKEN_2022_PROGRAM_ID
+    );
+    console.log("ATA: " + associatedTokenAddress.toBase58());
+    const transaction2 = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        associatedTokenAddress,
+        wallet.publicKey,
+        mintKeypair.publicKey,
+        TOKEN_2022_PROGRAM_ID
       )
     );
-    const latestBlockhash = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = latestBlockhash.blockhash;
-    transaction.feePayer = wallet.publicKey!;
-    transaction.partialSign(keyPair);
-    wallet.sendTransaction(transaction, connection);
+
+    await wallet.sendTransaction(transaction2, connection);
+
+    const transaction3 = new Transaction().add(
+      createMintToInstruction(
+        mintKeypair.publicKey,
+        associatedTokenAddress,
+        wallet.publicKey,
+        tokenSupply!,
+        [],
+        TOKEN_2022_PROGRAM_ID
+      )
+    );
+
+    await wallet.sendTransaction(transaction3, connection);
   };
   return (
     <div className="relative w-[90vw] md:w-[50vw] lg:w-[30vw] px-10 pt-7 pb-10 flex flex-col rounded-xl border border-zinc-700 bg-zinc-950">
@@ -63,6 +169,11 @@ function TokenLaunchpad() {
             <p>Connect your wallet</p>
             <img src="/arrow-tr.png" className="w-8 h-8 ml-3" />
           </div>
+        </div>
+      )}
+      {status && (
+        <div className="px-5 py-2 bg-zinc-700 rounded-lg font-bold">
+          {status}
         </div>
       )}
       <div className="text-2xl font-bold mb-8">Token details</div>
@@ -89,6 +200,18 @@ function TokenLaunchpad() {
           required
         />
 
+        <label htmlFor="description">Token description</label>
+        <input
+          className="mb-5 px-4 py-2 bg-zinc-800 rounded-lg"
+          type="text"
+          id="description"
+          placeholder="Describe your token briefly (max 150 characters)"
+          value={tokenDescription}
+          onChange={(e) => setTokenDescription(e.target.value)}
+          maxLength={150}
+          required
+        />
+
         <label htmlFor="initialSupply">Initial supply</label>
         <input
           className="mb-5 px-4 py-2 bg-zinc-800 rounded-lg"
@@ -101,11 +224,11 @@ function TokenLaunchpad() {
           min={1}
         />
 
-        <label htmlFor="initialSupply">Decimals</label>
+        <label htmlFor="decimals">Decimals</label>
         <input
           className="mb-5 px-4 py-2 bg-zinc-800 rounded-lg"
           type="number"
-          id="initialSupply"
+          id="decimals"
           placeholder="0-9 (9 common, 6 stable coins, 0 NFTs)"
           value={tokenDecimals}
           onChange={(e) => setTokenDecimals(Number(e.target.value))}
@@ -115,24 +238,26 @@ function TokenLaunchpad() {
         />
 
         <label>Token image</label>
-        <label
-          htmlFor="imageFile"
-          className="py-5 cursor-pointer rounded-lg flex items-center justify-center bg-zinc-800 hover:bg-zinc-800/80 transition-all duration-150"
-        >
-          <span className="font-bold">
-            {tokenImageFile?.name ? "Selected image : " : <Upload />}
-          </span>
-          <span className="ml-2 px-3 py-1 rounded-md bg-zinc-700">
-            {tokenImageFile?.name || "Upload image"}
-          </span>
-        </label>
-        <input
-          className="hidden"
-          type="file"
-          id="imageFile"
-          onChange={handleImageFileChange}
-          required
-        />
+        <div className="relative">
+          <label
+            htmlFor="imageFile"
+            className="py-5 cursor-pointer rounded-lg flex items-center justify-center bg-zinc-800 hover:bg-zinc-800/80 transition-all duration-150"
+          >
+            <span className="font-bold">
+              {tokenImageFile?.name ? "Selected image : " : <Upload />}
+            </span>
+            <span className="ml-2 px-3 py-1 rounded-md bg-zinc-700">
+              {tokenImageFile?.name || "Upload image"}
+            </span>
+          </label>
+          <input
+            className="absolute inset-1/3 opacity-0"
+            type="file"
+            id="imageFile"
+            onChange={handleImageFileChange}
+            required
+          />
+        </div>
         <button
           type="submit"
           className="bg-zinc-300/95 mt-5 hover:bg-zinc-400 cursor-pointer active:scale-[98%] 
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,34 +1,71 @@
 import axios from "axios";
 import { pinata } from "./pinata";
 
-export const uploadMetadataToIPFS = async (
-  publicKey: string,
-  fileType: string,
-  jsonMetadata: Object,
-  imageFile: File
-) => {
+export interface MetadataObject {
+  name: string;
+  symbol: string;
+  image: string;
+  description: string;
+}
+
+export interface MetadataUploadResponse {
+  message: string;
+  cid: string | null;
+}
+
+export interface MetadataUploadParams {
+  publicKey: string;
+  metadata: MetadataObject;
+  imageFile: File;
+}
+
+export const uploadMetadataToIPFS = async ({
+  publicKey,
+  metadata,
+  imageFile,
+}: MetadataUploadParams): Promise<MetadataUploadResponse> => {
   // get signedUrl for image
+
+  const fileType = imageFile.name.split(".").pop()?.toLocaleLowerCase() || "";
+  if (!fileType) {
+    return { message: "invalid/missing file type", cid: null };
+  }
   const res = await getSignedUrlForImage(publicKey, fileType);
   if (!res?.data.url)
-    return { message: "failed to get presigned url for image", url: null };
+    return { message: "failed to get presigned url for image", cid: null };
 
   // upload image to pinata
-  let imageCid = "";
+
   try {
     const upload = await pinata.upload.public.file(imageFile).url(res.data.url);
     if (upload.cid) {
-      imageCid = upload.cid;
+      metadata.image = `https://ipfs.io/ipfs/${upload.cid}`;
+      // console.log(metadata.image);
     } else {
-      return { message: "failed to upload image file", url: null };
+      return { message: "failed to upload image file", cid: null };
     }
   } catch (err: any) {
-    return { message: err, url: null };
+    return { message: `error: ${err}`, cid: null };
   }
 
-  // send json object to server to get back an IPFS uri
+  // upload metadata object to IPFS
 
-  // upload json object
-  // return onject uri
+  try {
+    const upload = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/uploadJSON`,
+      { metadata }
+    );
+    if (upload.data.cid) {
+      return {
+        message: "metadata successfully uploaded",
+        cid: upload.data.cid,
+      };
+    } else {
+      return { message: "failed to upload metadata json", cid: null };
+    }
+  } catch (err: any) {
+    return { message: `error: ${err}`, cid: null };
+  }
 };
 
 const getSignedUrlForImage = async (publicKey: string, fileType: string) => {
@@ -42,5 +79,3 @@ const getSignedUrlForImage = async (publicKey: string, fileType: string) => {
     return null;
   }
 };
-
-const getSignedUrlForMetadata = async () => {};
